c03937caceb5cb57f70d19f66df7abc26c02610a
0xKolyanus <mukola.1zt.@gmail.com>
2022-05-12 18:24:00 +0300

    change event bus: add del event and upsert

--- src/EventBus.Messages/Events/FilmsUpsertDtoEvent.cs
+++ src/EventBus.Messages/Events/FilmsUpsertDtoEvent.cs
@@ -2,12 +2,14 @@
 
 namespace EventBus.Messages.Events
 {
-    public class FilmsDtoEvent : IntegrationBaseEvent
+    public class FilmsUpsertDtoEvent : IntegrationBaseEvent
     {
-        public int? Id_Film { get; set; } = null;
+        public int Id_Film { get; set; }
         public string NameFilm { get; set; }
         public DateTime Data { get; set; }
         public string Country { get; set; }
         public int FKDescriptionId { get; set; }
+        public bool _is_add { get; set; }
+
     }
 }
