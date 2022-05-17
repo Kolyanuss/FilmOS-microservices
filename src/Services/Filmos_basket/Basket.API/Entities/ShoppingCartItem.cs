c52dae4e94c0c5fd3bb76387dbf1f607990f7058
0xKolyanus <mukola.1zt.@gmail.com>
2022-05-17 13:22:33 +0300

    add repository and controller, register them to startup

--- src/Services/Filmos_basket/Basket.API/Entities/ShoppingCartItem.cs
+++ src/Services/Filmos_basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -4,7 +4,7 @@
     {
         public int Quantity { get; set; }
         public decimal Price { get; set; }
-        public string ProductId { get; set; } // id in table
         public string ProductName { get; set; } // maybe like table name in sql db
+        public string ProductId { get; set; } // id in table
     }
 }
