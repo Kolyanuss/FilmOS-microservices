a3c2ac593177f29ce26e18756d81532b54afb527
0xKolyanus <mukola.1zt.@gmail.com>
2022-06-01 08:58:21 +0300

    create shopping grpc (server)

---
+++ src/Services/Filmos_shoping/Shoping.GRPC/Mapper/BasketProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Shoping.DAL.Entities.SQLEntities;
+using Shoping.GRPC.Protos;
+
+namespace Shoping.GRPC.Mapper
+{
+    public class BasketProfile : Profile
+    {
+        public BasketProfile()
+        {
+            CreateMap<SQLBasketFilms, BasketModel>().ReverseMap()
+                .ForMember(dest => dest.id_film, opt => opt.MapFrom(src => src.IdObject))
+                .ForMember(dest => dest.id_user, opt => opt.MapFrom(src => src.IdUser));
+            CreateMap<SQLBasketFilms, BasketFilmModel>().ReverseMap()
+                .ForMember(dest => dest.id_film, opt => opt.MapFrom(src => src.IdFilm))
+                .ForMember(dest => dest.id_user, opt => opt.MapFrom(src => src.IdUser));
+        }
+    }
+}
---
+++ src/Services/Filmos_shoping/Shoping.GRPC/Services/BasketService.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using Grpc.Core;
+using Shoping.DAL.Interfaces.SQLInterfaces.ISQLServices;
+using Shoping.GRPC.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shoping.GRPC.Services
+{
+    public class BasketService : BasketProto.BasketProtoBase
+    {
+        private readonly ISQLBasketFilmsService _SqlBasketServise;
+        private readonly IMapper _mapper;
+
+        public BasketService(ISQLBasketFilmsService sqlBasketServise, IMapper mapper)
+        {
+            _SqlBasketServise = sqlBasketServise ?? throw new ArgumentNullException(nameof(sqlBasketServise));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public override Task<BasketModel> GetTestFirstBasket(GetTestRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new BasketModel() { IdObject = 1, IdUser = 1 });
+        }
+
+        public override async Task GetAllBasket(GetAllBasketRequest request, IServerStreamWriter<BasketModel> responseStream, ServerCallContext context)
+        {
+            var sqlBasketList = await _SqlBasketServise.GetAllBasketFilms();
+            if (sqlBasketList == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Basket is empty."));
+            }
+
+            var basketModelList = _mapper.Map<IEnumerable<BasketModel>>(sqlBasketList);
+
+            foreach (var basket in basketModelList)
+            {
+                await responseStream.WriteAsync(basket);
+            }
+        }
+
+        public override async Task GetBasketFilmByUserName(GetBasketByNameUserRequest request, IServerStreamWriter<BasketFilmModel> responseStream, ServerCallContext context)
+        {
+            var IdUser = (await _SqlBasketServise.GetAllIdByUserName(request.UserName)).FirstOrDefault();
+
+            if (IdUser == 0)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"User with UserName={request.UserName} is not found."));
+            }
+
+            var sqlBasketList = await _SqlBasketServise.GetBasketByIdUser(IdUser);
+            if (sqlBasketList == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Basket for UserName={request.UserName} is empty."));
+            }
+
+            var basketModelList = _mapper.Map<IEnumerable<BasketFilmModel>>(sqlBasketList); //ERROR
+
+            foreach (var basket in basketModelList)
+            {
+                await responseStream.WriteAsync(basket);
+            }
+        }
+    }
+}
