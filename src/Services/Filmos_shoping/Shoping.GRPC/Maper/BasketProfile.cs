827958011d1c81a02a27aed716aeca994dff3140
0xKolyanus <mukola.1zt.@gmail.com>
2022-06-14 18:04:38 +0300

    fix mapper

--- src/Services/Filmos_shoping/Shoping.GRPC/Maper/BasketProfile.cs
+++ src/Services/Filmos_shoping/Shoping.GRPC/Maper/BasketProfile.cs
@@ -9,12 +9,16 @@ namespace Shoping.GRPC.Maper
     {
         public BasketProfile()
         {
-            /*CreateMap<SQLBasketFilms, BasketModel>().ReverseMap()
-                .ForMember(dest => dest.id_film, opt => opt.MapFrom(src => src.IdObject))
-                .ForMember(dest => dest.id_user, opt => opt.MapFrom(src => src.IdUser));*/
-            CreateMap<SQLBasketFilmsDTO, BasketModel>().ReverseMap()
+            CreateMap<BasketModel, SQLBasketFilmsDTO>()
                 .ForMember(dest => dest.id_film, opt => opt.MapFrom(src => src.IdObject))
                 .ForMember(dest => dest.id_user, opt => opt.MapFrom(src => src.IdUser));
+            CreateMap<SQLBasketFilms, BasketModel>()
+                .ForMember(dest => dest.IdObject, opt => opt.MapFrom(src => src.id_film))
+                .ForMember(dest => dest.IdUser, opt => opt.MapFrom(src => src.id_user))
+                .ForMember(dest => dest.TypeObject, opt => opt.MapFrom(_ => "Film"))
+                .ForMember(dest => dest.Price, opt => opt.MapFrom(_ => 111)) // stub
+                .ForMember(dest => dest.Quantity, opt => opt.MapFrom(_ => 1)); // stub
+
             CreateMap<SQLBasketFilmsDTO, SQLBasketFilms>().ReverseMap();
         }
     }
