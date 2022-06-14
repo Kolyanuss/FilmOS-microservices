877155b7b244d6515991304df676ba51a0857ef9
0xKolyanus <mukola.1zt.@gmail.com>
2022-06-14 18:04:53 +0300

    fix second mapper

--- src/Services/Filmos_basket/Basket.API/Mapper/BasketProfile.cs
+++ src/Services/Filmos_basket/Basket.API/Mapper/BasketProfile.cs
@@ -8,13 +8,12 @@ namespace Basket.API.Mapper
 	{
 		public BasketProfile()
 		{
-			//CreateMap<BasketCheckout, BasketCheckoutEvent>().ReverseMap(); // add in future
-			CreateMap<ShoppingCartItem, BasketModel>().ReverseMap()
-				.ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.IdObject))
-				.ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.TypeObject));
-			CreateMap<ShoppingCartItem, BasketModel>()
-				.ForMember(dest => dest.IdObject, opt => opt.MapFrom(src => src.ProductId))
-				.ForMember(dest => dest.TypeObject, opt => opt.MapFrom(src => src.ProductName));
-		}
+            CreateMap<ShoppingCartItem, BasketModel>()
+                .ForMember(dest => dest.IdObject, opt => opt.MapFrom(src => src.ProductId))
+                .ForMember(dest => dest.TypeObject, opt => opt.MapFrom(src => src.ProductName));
+            CreateMap<BasketModel, ShoppingCartItem>()
+                .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.IdObject))
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.TypeObject));
+        }
 	}
 }
