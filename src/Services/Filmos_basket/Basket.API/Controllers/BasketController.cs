8b05f0f124d7f9df0d1c1f231b429b11c94a9e88
0xKolyanus <mukola.1zt.@gmail.com>
2022-06-14 18:19:13 +0300

    modify controller

--- src/Services/Filmos_basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Filmos_basket/Basket.API/Controllers/BasketController.cs
@@ -15,24 +15,11 @@ namespace Basket.API.Controllers
     {
         private readonly IBasketRepository _repository;
         private readonly BasketGrscService _grpc;
-        private List<ShoppingCart> _listToDelete;
-        private List<string> _listIdToAdd;
 
         public BasketController(IBasketRepository repository, BasketGrscService grpc)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
             _grpc = grpc ?? throw new ArgumentNullException(nameof(grpc));
-            UpdateBasket_Get();
-            _listToDelete = new List<ShoppingCart>();
-            _listIdToAdd = new List<string>();
-        }
-
-        protected async Task UpdateBasket_Get()
-        {
-            foreach (var item in await _grpc.GetAllBasket())
-            {
-                await _repository.UpdateBasket(item);
-            }
         }
 
         [HttpGet("{userId}", Name = "GetBasket")]
@@ -55,23 +42,38 @@ namespace Basket.API.Controllers
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> EditBasket([FromBody] ShoppingCart basket)
         {
-            var addGrpc = await _grpc.AddCardInBasket(basket);
-            var addRepo = await _repository.UpdateBasket(basket);
-            if (addGrpc && addRepo != null)
+            var RepoStatus = await _repository.UpdateBasket(basket);
+            var GrpcStatus = await _grpc.AddCardInBasket(basket);
+            if (GrpcStatus && RepoStatus != null)
             {
-                return Ok(addRepo);
+                return Ok(RepoStatus);
             }
-            else return BadRequest("status grpc - " + addGrpc);
+            else return BadRequest("status grpc - " + GrpcStatus + ", status redis - " + RepoStatus);
         }
 
-        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        [HttpDelete("{userId}", Name = "DeleteBasket")]
         [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> DeleteBasket(int userId)
         {
             var id = (userId.ToString());
-            await _grpc.DeleteCardInBasket(await _repository.GetBasket(id));
-            await _repository.DeleteBasket(id);
-            return Ok();
+            var GrpcStatus = await _grpc.DeleteCardInBasket(await _repository.GetBasket(id)); // FIRST GRPC!!!
+            await _repository.DeleteBasket(id); // second repo
+            if (GrpcStatus)
+            {
+                return Ok();
+            }
+            else return BadRequest("status grpc - " + GrpcStatus);
+        }
+
+
+        [HttpGet("GetAllFromSql")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task UpdateBasket_Get()
+        {
+            foreach (var item in await _grpc.GetAllBasket())
+            {
+                await _repository.UpdateBasket(item);
+            }
         }
     }
 }
