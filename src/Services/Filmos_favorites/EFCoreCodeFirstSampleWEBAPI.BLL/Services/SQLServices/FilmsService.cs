e33e44491408a876bd7289670f57f80c56de1be0
0xKolyanus <mukola.1zt.@gmail.com>
2022-06-16 12:26:31 +0300

    add serilog to favorite project, add logger to controller, servise and repo

--- src/Services/Filmos_favorites/EFCoreCodeFirstSampleWEBAPI.BLL/Services/SQLServices/FilmsService.cs
+++ src/Services/Filmos_favorites/EFCoreCodeFirstSampleWEBAPI.BLL/Services/SQLServices/FilmsService.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using EventBus.Messages.Events;
+using Microsoft.Extensions.Logging;
 
 namespace EFCoreCodeFirstSampleWEBAPI.BLL.Services.SQLServices
 {
@@ -19,50 +20,64 @@ namespace EFCoreCodeFirstSampleWEBAPI.BLL.Services.SQLServices
         private IRepositoryWrapper _wraper;
         private IMapper _mapper;
         private readonly IPublishEndpoint _publishEndpoint;
+        private readonly ILogger<FilmsService> _logger;
 
-        public FilmsService(IRepositoryWrapper wraper, IMapper mapper, IPublishEndpoint publishEndpoint)
+        public FilmsService(IRepositoryWrapper wraper, IMapper mapper, IPublishEndpoint publishEndpoint, ILogger<FilmsService> logger)
         {
             _wraper = wraper;
             _mapper = mapper;
             _publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
         public async Task<IEnumerable<FilmsDTO>> GetAll()
         {
+            _logger.LogInformation("In " + this.GetType() + " call GetAllAsync");
             IEnumerable<Films> Filmes = await _wraper.Films.GetAllAsync();
-            return _mapper.Map<IEnumerable<FilmsDTO>>(Filmes);
+            var map = _mapper.Map<IEnumerable<FilmsDTO>>(Filmes);
+            _logger.LogInformation("return maps " + map.GetType() + ": ", map);
+            return map;
         }
 
         public async Task<FilmsDTO> GetById(int id)
         {
+            _logger.LogInformation("In " + this.GetType() + " call GetByIdAsync");
             var films = await _wraper.Films.GetByIdAsync(id);
 
             if (films == null)
             {
                 throw new FilmsNotFoundException(id);
             }
-            return _mapper.Map<FilmsDTO>(films);
+            var map = _mapper.Map<FilmsDTO>(films);
+            _logger.LogInformation("return maps " + map.GetType() + ": ", map);
+            return map;
         }
 
         public async Task<FilmsDTO> GetByIdSpec(int id)
         {
             var specification = new GetFilmByIdAsync(id);
+            _logger.LogInformation("In " + this.GetType() + " call FindWithSpecificationPattern");
             var films = _wraper.Films.FindWithSpecificationPattern(specification);
 
             if (films == null)
             {
                 throw new FilmsNotFoundException(id);
             }
-            return _mapper.Map<FilmsDTO>(films);
+            var map = _mapper.Map<FilmsDTO>(films);
+            _logger.LogInformation("return maps " + map.GetType() + ": ", map);
+            return map;
         }
 
         public async Task<FilmsDetailDTO> GetWithDetailsById(int id)
         {
+            _logger.LogInformation("In " + this.GetType() + " call GetByIdWithDetailsAsync");
             var films = await _wraper.Films.GetByIdWithDetailsAsync(id);
             if (films == null)
             {
                 throw new FilmsNotFoundException(id);
             }
-            return _mapper.Map<FilmsDetailDTO>(films);
+            var map = _mapper.Map<FilmsDetailDTO>(films);
+            _logger.LogInformation("return maps " + map.GetType() + ": ", map);
+            return map;
         }
 
         public async Task<FilmsDTO> Post(FilmsForCreationDto filmsDto)
@@ -77,15 +92,19 @@ namespace EFCoreCodeFirstSampleWEBAPI.BLL.Services.SQLServices
             }
 
             var films = _mapper.Map<Films>(filmsDto);
+            _logger.LogInformation("In " + this.GetType() + " call Add");
             await _wraper.Films.Add(films);
 
             // send add event to rabbitmq
             var eventMessage = _mapper.Map<FilmsUpsertDtoEvent>(filmsDto);
+            _logger.LogInformation("send " + eventMessage.GetType() + " message to rabbitMQ");
             eventMessage.Id_Film = films.Id;
             eventMessage._is_add = true;
             await _publishEndpoint.Publish<FilmsUpsertDtoEvent>(eventMessage);
 
-            return _mapper.Map<FilmsDTO>(films);
+            var map = _mapper.Map<FilmsDTO>(films);
+            _logger.LogInformation("return maps " + map.GetType() + ": ", map);
+            return map;
         }
 
         public async Task Put(int id, FilmsForCreationDto filmsDto)
@@ -104,10 +123,12 @@ namespace EFCoreCodeFirstSampleWEBAPI.BLL.Services.SQLServices
                 throw new FilmsNotFoundException(id);
             }
             _mapper.Map(filmsDto, ToUpdate);
+            _logger.LogInformation("In " + this.GetType() + " call Update");
             _wraper.Films.Update(ToUpdate);
 
             // send update event to rabbitmq
             var eventMessage = _mapper.Map<FilmsUpsertDtoEvent>(filmsDto);
+            _logger.LogInformation("send " + eventMessage.GetType() + " message to rabbitMQ");
             eventMessage.Id_Film = id;
             eventMessage._is_add = false;
             await _publishEndpoint.Publish<FilmsUpsertDtoEvent>(eventMessage);
@@ -120,10 +141,12 @@ namespace EFCoreCodeFirstSampleWEBAPI.BLL.Services.SQLServices
             {
                 throw new FilmsNotFoundException(id);
             }
+            _logger.LogInformation("In " + this.GetType() + " call Delete");
             _wraper.Films.Delete(films);
 
             // send delete event to rabbitmq
             var eventMessage = new FilmsDeleteDtoEvent() { Id_Film = id };
+            _logger.LogInformation("send " + eventMessage.GetType() + " message to rabbitMQ");
             await _publishEndpoint.Publish<FilmsDeleteDtoEvent>(eventMessage);
         }
     }
--- src/Services/Filmos_favorites/EFCoreCodeFirstSampleWEBAPI.DAL/Repository/RepositoryBase.cs
+++ src/Services/Filmos_favorites/EFCoreCodeFirstSampleWEBAPI.DAL/Repository/RepositoryBase.cs
@@ -1,6 +1,7 @@
 ﻿using EFCoreCodeFirstSampleWEBAPI.DAL.Interfaces;
 using EFCoreCodeFirstSampleWEBAPI.DAL.Specifications;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,23 +13,33 @@ namespace EFCoreCodeFirstSampleWEBAPI.DAL.Repository
     public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
     {
         protected MyAppContext MyAppContext { get; set; }
+        private readonly ILogger<RepositoryBase<T>> _logger;
         public RepositoryBase(MyAppContext myAppContext)
         {
             MyAppContext = myAppContext;
+            _logger = null;
+        }
+        public RepositoryBase(MyAppContext myAppContext, ILogger<RepositoryBase<T>> logger)
+        {
+            MyAppContext = myAppContext;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public IQueryable<T> GetAll()
         {
+            _logger.LogInformation("In " + this.GetType() + " work GetAll");
             return MyAppContext.Set<T>().AsNoTracking();
         }
 
         public IQueryable<T> GetByCondition(Expression<Func<T, bool>> expression)
         {
+            _logger.LogInformation("In " + this.GetType() + " work GetByCondition");
             return MyAppContext.Set<T>().Where(expression);
         }
 
         public IEnumerable<T> FindWithSpecificationPattern(ISpecification<T> specification = null)
         {
+            _logger.LogInformation("In " + this.GetType() + " work FindWithSpecificationPattern");
             return SpecificationEvaluator<T>.GetQuery(MyAppContext.Set<T>().AsQueryable(), specification);
         }
 
@@ -36,18 +47,21 @@ namespace EFCoreCodeFirstSampleWEBAPI.DAL.Repository
         {
             await MyAppContext.Set<T>().AddAsync(entity);
             MyAppContext.SaveChanges();
+            _logger.LogInformation("In " + this.GetType() + " SaveChanges AddAsync");
         }
 
         public void Update(T entity)
         {
             MyAppContext.Set<T>().Update(entity);
             MyAppContext.SaveChanges();
+            _logger.LogInformation("In " + this.GetType() + " SaveChanges Update");
         }
 
         public void Delete(T entity)
         {
             MyAppContext.Set<T>().Remove(entity);
             MyAppContext.SaveChanges();
+            _logger.LogInformation("In " + this.GetType() + " SaveChanges Remove");
         }
     }
 }
