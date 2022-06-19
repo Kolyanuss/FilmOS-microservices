02f2a8c5b5d0ff168f72ff32e2192661e37c8f0d
0xKolyanus <mukola.1zt.@gmail.com>
2022-06-19 12:24:52 +0300

    added serilog and elastic config to a separate file, added test log to rating controller

--- src/Services/Filmos_favorites/EFCoreCodeFirstSampleWEBAPI/Program.cs
+++ src/Services/Filmos_favorites/EFCoreCodeFirstSampleWEBAPI/Program.cs
@@ -2,9 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
-using Serilog.Sinks.Elasticsearch;
 using System;
-using System.Reflection;
 
 namespace EFCoreCodeFirstSampleWEBAPI
 {
@@ -12,9 +10,9 @@ namespace EFCoreCodeFirstSampleWEBAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
             //Read Configuration from appSettings
-            /*var config = new ConfigurationBuilder()
+            var config = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
                 .Build();
             //Initialize Logger
@@ -33,28 +31,12 @@ namespace EFCoreCodeFirstSampleWEBAPI
             finally
             {
                 Log.CloseAndFlush();
-            }*/
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseSerilog((context, configuration) =>
-                {
-                    configuration
-                    .Enrich.FromLogContext()
-                    .Enrich.WithMachineName()
-                    .WriteTo.Console()
-                    .WriteTo.Elasticsearch(
-                    new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"]))
-                    {
-                        IndexFormat = $"applogs -{ Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-") }-{ context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-") }-logs -{ DateTime.UtcNow:yyyy-MM}",
-                        AutoRegisterTemplate = true,
-                        NumberOfShards = 2,
-                        NumberOfReplicas = 1
-                    })
-                    .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
-                    .ReadFrom.Configuration(context.Configuration);
-                })
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
