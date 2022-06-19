f1f19f0df3dcf995c705be84fca29e6452a51d19
0xKolyanus <mukola.1zt.@gmail.com>
2022-06-19 21:15:59 +0300

    scopes added

---
+++ src/SecureResource/IdentityServer/Config.cs
@@ -0,0 +1,44 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            new Client
+            {
+                ClientId = "favoritesClient",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = {
+                    new Secret("secret".Sha256())
+                },
+                AllowedScopes = { "EFCoreCodeFirstSampleWEBAPI" }
+            }
+        };
+        public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new ApiScope("EFCoreCodeFirstSampleWEBAPI", "Filmos Favorites API")
+        };
+        public static IEnumerable<ApiResource> ApiResources =>
+        new ApiResource[]
+        {
+        };
+        public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+        };
+        public static List<TestUser> TestUsers =>
+        new List<TestUser>
+        {
+        };
+    }
+}
--- src/SecureResource/IdentityServer/Startup.cs
+++ src/SecureResource/IdentityServer/Startup.cs
@@ -1,33 +1,29 @@
+using IdentityServer4.Models;
+using IdentityServer4.Test;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace IdentityServer
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddIdentityServer();
+            //services.AddControllersWithViews();
 
-            services.AddControllers();
+            // IdentityServer config
+            services.AddIdentityServer()
+                .AddInMemoryClients(Config.Clients)
+                //.AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                //.AddTestUsers(Config.TestUsers)
+                .AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,17 +34,18 @@ namespace IdentityServer
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
+            //app.UseStaticFiles();
             app.UseRouting();
-
             app.UseIdentityServer();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
         }
     }
